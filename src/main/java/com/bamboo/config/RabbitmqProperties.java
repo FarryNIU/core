50ca57737fff7d21f8d192a9553b02043c2a8101
FrrayNIU <997643118@qq.com>
2025-12-08 00:16:09 +0800

    feat:增加rabbitmq配置文件

---
+++ src/main/java/com/bamboo/config/RabbitmqProperties.java
@@ -0,0 +1,19 @@
+package com.bamboo.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author FarryNiu 2025/12/8
+ */
+@Component
+@ConfigurationProperties(prefix = "bamboo.rabbitmq")
+@Data
+public class RabbitmqProperties {
+    private String host;
+    private int port;
+    private String virtualHost;
+    private String username;
+    private String password;
+}
